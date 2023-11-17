91a8a39013bd2759447f1bf4ea417252aa3dabfb
Simon Burkhardt <simon@simonmartin.ch>
2023-11-17 07:21:56 +0100

    remove all FPGA project files

--- SSD_Test.vitis/create_platform.tcl
+++
@@ -1,2 +0,0 @@
-platform create -name "SSD_Test_platform" -hw "../SSD_Test_wrapper.xsa" -proc psu_cortexa53_0 -os standalone
-platform generate
--- SSD_Test.vitis/create_projects.tcl
+++
@@ -1,5 +0,0 @@
-file copy -force "SSD_Test/src/lscript.ld" "lscript_temp.ld"
-app create -name "SSD_Test" -template {Empty Application(C)}
-app config -name "SSD_Test" libraries m
-file copy -force "lscript_temp.ld" "SSD_Test/src/lscript.ld" 
-file delete -force "lscript_temp.ld"
